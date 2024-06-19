433e5910dfb5ed6f7c1881413b9e3687df350bba
SoumyaPanda <soumyaranjanpanda988@gmail.com>
2024-06-19 06:36:05 +0530

    first commit

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,131 @@
+import { Hono } from "hono";
+import { verify } from 'hono/jwt'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { createPostInput, updatePostInput } from '@soumrnjn/connectly-common/dist'
+
+
+export const blogRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string,
+        JWT_SECRET: string,
+    },
+    Variables: {
+        userId: string
+    }
+}>();
+
+
+blogRouter.use('/*', async (c, next) => {
+    const jwt = await c.req.header('Authorization');
+    try {
+        if (!jwt) {
+            c.status(401);
+            return c.json({ error: "unauthorized" });
+        }
+        const token = jwt.split(' ')[1];
+        const payload = await verify(token, c.env.JWT_SECRET);
+
+        if (!payload) {
+            c.status(401);
+            return c.json({ error: "unauthorized" });
+        }
+        c.set('userId', payload.id as string);
+        await next()
+    } catch (error) {
+        c.status(403);
+        return c.json({ error: "unauthorized" });
+    }
+})
+
+blogRouter.post('/', async (c) => {
+    const body = await c.req.json()
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const { success } = createPostInput.safeParse(body);
+    if (!success) {
+        c.status(400);
+        return c.json({ error: "invalid input" });
+    }
+
+    const post = await prisma.post.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            published: true,
+            author_id: c.get('userId')
+        }
+    })
+    return c.json({
+        id: post.id
+    })
+})
+
+blogRouter.put('/', async (c) => {
+    const body = await c.req.json()
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const { success } = updatePostInput.safeParse(body);
+    if (!success) {
+        c.status(400);
+        return c.json({ error: "invalid input" });
+    }
+
+    const post = await prisma.post.update({
+        where: {
+            id: body.id
+        },
+        data: {
+            title: body.title,
+            content: body.content
+        }
+    })
+    return c.json({
+        id: post.id,
+        post: post,
+        updated: true
+    })
+})
+
+blogRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const posts = await prisma.post.findMany()
+        return c.json({ posts })
+    } catch (error) {
+        c.status(403)
+        return c.json({
+            msg: "Error while fetching posts"
+        })
+    }
+})
+
+blogRouter.get('/:id', async (c) => {
+    const id = await c.req.param('id')
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const post = await prisma.post.findFirst({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        return c.json({ post })
+    } catch (error) {
+        c.status(403)
+        return c.json({
+            msg: "Error while fetching post"
+        })
+    }
+})
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzMTk3NjRmLWUwOTgtNGJlMS04MjM1LTc0ZTBlYzI4YjA4YSJ9.OsFUAkxOg4uQvGZkd4UZt-FFD_5o17VjFDYOe59OckA
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI0ZTRmOWQ3LWZlNTctNDM5OS04MTMxLTVlMDlmYmRmMWY0MyJ9.fM2Fb2HIlumIO36nbs-l3WhFOzqcoMaxBq0k-ipPRKI
