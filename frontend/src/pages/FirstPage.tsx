3064c754199346c782f4454e899dd2f0f20fc1b1
SoumyaPanda <soumyaranjanpanda988@gmail.com>
2024-06-21 02:56:27 +0530

    Add github link

--- frontend/src/pages/FirstPage.tsx
+++ frontend/src/pages/FirstPage.tsx
@@ -61,6 +61,15 @@ export default function Component() {
                                         </Link>
                                     </div>
                                 )}
+                                <div>
+                                    <a
+                                        type="button"
+                                        href="https://github.com/soumyaranjan-panda/Connectly" target="_blank"
+                                        className="text-white bg-gray-800 hover:bg-gray-900   font-medium rounded-full text-sm px-5 py-2.5 me-2  "
+                                    >
+                                        GitHub
+                                    </a>
+                                </div>
                             </div>
                             <img
                                 src={img}
